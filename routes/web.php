5f89b624bc6c5f70af0105990906175f69bcdf2f
Dilla Maulidia <p3rs3ph0n03@gmail.com>
2026-06-11 16:29:57 +0700

    Menambahkan interface web pengelolaan ebook

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/ebooks', function () {
+    return view('ebooks.index');
+});
+
+Route::get('/ebooks/create', function () {
+    return view('ebooks.create');
+});
+
+Route::get('/categories', function () {
+    return view('categories.index');
 });
