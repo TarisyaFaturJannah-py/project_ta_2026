bdf0e1d4035e73a75a1ab5d681fab88d52474cc3
M. Hilmy Helsinky <yduta21@gmail.com>
2026-06-12 11:35:25 +0700

    fixing

--- app/Contracts/Contracts/OpenLibraryServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Contracts;
-
-interface OpenLibraryServiceInterface
-{
-    // Fungsi untuk mencari buku berdasarkan kode ISBN
-    public function getBookByIsbn(string $isbn);
-}
