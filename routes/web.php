f144fe84c8987957e287eddac15198f825198320
M. Hilmy Helsinky <yduta21@gmail.com>
2026-06-12 11:57:10 +0700

    fixing

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+use App\Http\Controllers\EbookController; // Pastikan ini ada
 
-=======
-use App\Http\Controllers\EbookController; // <-- INI BARIS YANG HILANG TADI
-
-// Halaman Utama / Dashboard User
->>>>>>> 3830aa6 (first commit)
 Route::get('/', function () {
     return view('dashboard.user');
 });
 
-<<<<<<< HEAD
-=======
-// Halaman Dashboard Admin
->>>>>>> 3830aa6 (first commit)
 Route::get('/admin', function () {
     return view('dashboard.admin');
 });
 
-<<<<<<< HEAD
-=======
-// Halaman Login
->>>>>>> 3830aa6 (first commit)
 Route::get('/login', function () {
     return view('auth.login');
 });
 
-<<<<<<< HEAD
-=======
-// Halaman Register
->>>>>>> 3830aa6 (first commit)
 Route::get('/register', function () {
     return view('auth.register');
 });
 
-<<<<<<< HEAD
-=======
-// Halaman Profile
->>>>>>> 3830aa6 (first commit)
 Route::get('/profile', function () {
     return view('profile.index');
 });
 
-<<<<<<< HEAD
-Route::get('/ebooks', function () {
-    return view('ebooks.index');
-});
-
-=======
-// Halaman E-Books (Rute buatanmu sebelumnya)
+// Pintu halaman Ebook
 Route::get('/ebooks', [EbookController::class, 'index']);
 
-// Rute lainnya dari frontend
->>>>>>> 3830aa6 (first commit)
+// Pintu frontend lainnya
 Route::get('/ebooks/create', function () {
     return view('ebooks.create');
 });
@@ -69,11 +41,7 @@ Route::get('/categories', function () {
 
 Route::get('/dashboard-user', function () {
     return view('dashboard.user');
-<<<<<<< HEAD
-});
-=======
 });
 
-// Rute untuk mengetes fitur Open Library API
+// Pintu untuk mengetes API
 Route::get('/cek-buku', [EbookController::class, 'cekBukuDariInternet']);
->>>>>>> 3830aa6 (first commit)
